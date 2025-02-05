67ece722f6e7b59e5284bf4feb55d912d6734036
Ren-Gen22 <abhishekrenjan33@gmail.com>
2025-02-05 20:03:54 +0530

    added usful pkg

--- yayPckgs.sh
+++ yayPckgs.sh
@@ -4,5 +4,5 @@ echo "Yay packages require manual password entry pls check"
 git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si --noconfirm
 cd ..
 rm -rf yay
-
+# networkmanager-dmenu-git
 yay -S --noconfirm spaceship-prompt wlogout hyprpolkitagent
